73d953ad48e75a2dc0d82bb49eefc58de2b0b74c
Fernando Noguera <jfnoguerab@gmail.com>
2024-11-26 18:03:35 -0500

    Se agregaron las funcionalidades de 'prestar' y 'devolver' en la clase 'biblioteca' y se creó la clase 'Persona'

--- models/Biblioteca.java
+++ models/Biblioteca.java
@@ -4,11 +4,53 @@ import java.util.Arrays;
 
 public class Biblioteca {
     private Libro[] libros = new Libro[1];
+    private Persona[] personas = new Persona[1];
 
     public Biblioteca() {
     }
 
+    public void prestar(Libro libro, Persona persona) {
+        // Validamos que existan libros
+        if (hasLibros() && hasPersonas() && libro != null && persona != null) {
+            // Buscamos el libro en el arreglo de libros
+            Integer indexLibro = existElement(libro);
+            if (indexLibro != null) {
+                // Buscamos la persona en el arreglo de personas
+                Integer indexPersona = existElement(persona);
+                if (indexPersona != null) {
+                    libros[indexLibro].prestar();
+                    if (libros[indexLibro].isPrestado()) {
+                        personas[indexPersona].addLibroPrestado(libros[indexLibro]);
+                    }
+                }
+            }
+        }
+
+    }
     
+    public void devolver(Libro libro, Persona persona) {
+        // Validamos que existan libros y personas
+        if (hasLibros() && hasPersonas() && libro != null && persona != null) {
+            // Buscamos el libro en el arreglo de libros
+            Integer indexLibro = existElement(libro);
+            if (indexLibro != null) {
+                // Buscamos la persona en el arreglo de personas
+                Integer indexPersona = existElement(persona);
+                if (indexPersona != null) {
+                    // Validamos que el ususario sea el mismo que tenia prestado el libro
+                    if (personas[indexPersona].existElement(libro) != null) {
+                        libros[indexLibro].devolver();
+                        if (!libros[indexLibro].isPrestado()) {
+                            personas[indexPersona].removeLibroPrestado(libros[indexLibro]);
+                        }
+                    } else {
+                        System.out.println("Lo sentimos, La persona no tiene el libro prestado.");
+                    }
+                }
+            }
+        }
+
+    }
 
     public Libro[] addLibro(Libro libro) {
         if (libro != null) {
@@ -29,5 +71,47 @@ public class Biblioteca {
 
     public boolean hasLibros() {
         return libros != null && libros[0] != null;
+    }
+
+    // Busca un libro dentro del arreglo de libros
+    // y devuelve el indice, sino existe devuelve null
+    public Integer existElement(Libro libro) {
+        for (int i = 0; i < libros.length; i++) {
+            if (libros[i].equalsIgnoreCase(libro)) {
+                return i;
+            }
+        }
+        return null;
+    }
+    // Busca una persona dentro del arreglo de personas
+    // y devuelve el indice, sino existe devuelve null
+    public Integer existElement(Persona persona) {
+        for (int i = 0; i < personas.length; i++) {
+            if (personas[i].equalsIgnoreCase(persona)) {
+                return i;
+            }
+        }
+        return null;
+    }
+    
+    public Persona[] addPersona(Persona persona) {
+        if (persona != null) {
+            // Si ya existe algún valor en el arreglo entoces aumentamos en 1 el tamaño
+            if (personas != null && personas[0] != null) {
+                personas = Arrays.copyOf(personas, personas.length + 1);
+            }
+
+            personas[personas.length - 1] = persona;
+        }
+
+        return personas;
+    }
+
+    public Persona[] getPersonas() {
+        return personas;
+    }
+
+    public boolean hasPersonas() {
+        return personas != null && personas[0] != null;
     }
 }
--- models/Libro.java
+++ models/Libro.java
@@ -94,4 +94,63 @@ public class Libro extends ItemBiblioteca implements Catalogable {
         this.prestado = prestado;
     }
 
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((titulo == null) ? 0 : titulo.hashCode());
+        result = prime * result + ((autor == null) ? 0 : autor.hashCode());
+        result = prime * result + numeroDePaginas;
+        return result;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Libro other = (Libro) obj;
+        if (titulo == null) {
+            if (other.titulo != null)
+                return false;
+        } else if (!titulo.equals(other.titulo))
+            return false;
+        if (autor == null) {
+            if (other.autor != null)
+                return false;
+        } else if (!autor.equals(other.autor))
+            return false;
+        if (numeroDePaginas != other.numeroDePaginas)
+            return false;
+        return true;
+    }
+    
+    public boolean equalsIgnoreCase(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Libro other = (Libro) obj;
+        if (titulo == null) {
+            if (other.titulo != null)
+                return false;
+        } else if (!titulo.equalsIgnoreCase(other.titulo))
+            return false;
+        if (autor == null) {
+            if (other.autor != null)
+                return false;
+        } else if (!autor.equalsIgnoreCase(other.autor))
+            return false;
+        if (numeroDePaginas != other.numeroDePaginas)
+            return false;
+        return true;
+    }
+
 }
